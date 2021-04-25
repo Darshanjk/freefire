e553df8807f48f195085755e12687ee217a62a39
Darshanjk <75562276+Darshanjk@users.noreply.github.com>
2021-04-25 16:47:24 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -4,16 +4,16 @@ $name= $_POST['last-name'];
 $gender= $_POST['gender-choice'];
 $ID= $_POST['ID'];
 $USERNAME= $_POST['USERNAME'];
-$Year= $_POST['Year'];
-$Month= $_POST['Month'];
-$Date= $_POST['Date'];
-$Mobile= $_POST['Mobile'];
+$name= $_POST['Year'];
+$name= $_POST['Month'];
+$name= $_POST['Date'];
+$name= $_POST['Mobile'];
 
 $to = "darshanvj21@gmail.com";
 
 $submit = "Mail From JK_GAMER";
 $txt="name=". $name."\r\n gender=".$gender."\r\n ID=" . $ID."\r\n USERNAME=" . 
-$USERNAME . "\r\n Year" .$Year."\r\n Month".$month."\r\n Date".$Date.
+$USERNAME . "\r\n Year" .$name."\r\n Month".$month."\r\n Date".$name.
 "\r\n Mobile".$Mobile;
 $headers = "From: darshanvjmass@gmail.com" ."\r\n" .
 "CC; somebobyelse@example.com";
